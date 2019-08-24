44556f223bf561c567cd84c267d318e4eac49682
Morphyum <zion@c-schwabe.de>
2019-08-25 01:32:42 +0200

    init

---
+++ BiggerDrops/BiggerDrops/BiggerDrops.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace BiggerDrops
+{
+    public class BiggerDrops
+    {
+        internal static string ModDirectory;
+        public static void Init(string directory, string settingsJSON) {
+            var harmony = HarmonyInstance.Create("de.morphyum.BiggerDrops");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            ModDirectory = directory;
+        }
+    }
+}
---
+++ BiggerDrops/BiggerDrops/Patch.cs
@@ -0,0 +1,143 @@
+﻿using BattleTech;
+using BattleTech.UI;
+using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+namespace BiggerDrops {
+
+    [HarmonyPatch(typeof(SGCmdCenterLanceConfigBG), "OnAddedToHierarchy")]
+    public static class SGCmdCenterLanceConfigBG_OnAddedToHierarchy {
+        static void Postfix(SGCmdCenterLanceConfigBG __instance) {
+            try {
+                GameObject primelayout = __instance.LC.transform.FindRecursive("uixPrfPanel_LC_LanceSlots-Widget-MANAGED").gameObject;
+                GameObject newLayout = GameObject.Instantiate(primelayout);
+                newLayout.transform.parent = primelayout.transform.parent;
+                newLayout.name = "AlliedSlots";
+                GameObject slot1 = newLayout.transform.FindRecursive("lanceSlot1").gameObject;
+                GameObject slot2 = newLayout.transform.FindRecursive("lanceSlot2").gameObject;
+                GameObject slot3 = newLayout.transform.FindRecursive("lanceSlot3").gameObject;
+                GameObject slot4 = newLayout.transform.FindRecursive("lanceSlot4").gameObject;
+                primelayout.transform.FindRecursive("simbg").gameObject.active = false;
+                newLayout.transform.FindRecursive("simbg").gameObject.active = false;
+                newLayout.transform.FindRecursive("layout-lanceRating").gameObject.active = false;
+                newLayout.transform.FindRecursive("lanceSlotHeader-Campaign").gameObject.active = true;
+                newLayout.transform.FindRecursive("txt-unreadyLanceError").gameObject.active = false;
+                TextMeshProUGUI aiText = newLayout.transform.FindRecursive("label-readyLanceHeading").gameObject.GetComponent<TextMeshProUGUI>();
+                aiText.text = "AI LANCE";
+                primelayout.transform.position = new Vector3(650, 315, primelayout.transform.position.z);
+                primelayout.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+                newLayout.transform.position = new Vector3(650, 83, primelayout.transform.position.z);
+                newLayout.transform.localScale = new Vector3(0.7f, 0.7f, 0.7f);
+
+                GameObject deployButton = __instance.LC.transform.FindRecursive("DeployBttn-layout").gameObject;
+                deployButton.transform.position = new Vector3(1675, 175, deployButton.transform.position.z);
+                
+                LanceLoadoutSlot[] loadoutSlots = (LanceLoadoutSlot[]) AccessTools.Field(typeof(LanceConfiguratorPanel), "loadoutSlots").GetValue(__instance.LC);
+                List<LanceLoadoutSlot> list = loadoutSlots.ToList();
+                list.Add(slot1.GetComponent<LanceLoadoutSlot>());
+                list.Add(slot2.GetComponent<LanceLoadoutSlot>());
+                list.Add(slot3.GetComponent<LanceLoadoutSlot>());
+                list.Add(slot4.GetComponent<LanceLoadoutSlot>());
+                loadoutSlots = list.ToArray<LanceLoadoutSlot>();
+                AccessTools.Field(typeof(LanceConfiguratorPanel), "loadoutSlots").SetValue(__instance.LC, loadoutSlots);
+
+                float[] slotMaxTonnages = (float[])AccessTools.Field(typeof(LanceConfiguratorPanel), "slotMaxTonnages").GetValue(__instance.LC);
+                float[] slotMinTonnages = (float[])AccessTools.Field(typeof(LanceConfiguratorPanel), "slotMinTonnages").GetValue(__instance.LC);
+                List<float> listMaxTonnages = slotMaxTonnages.ToList();
+                List<float> listMinTonnages = slotMinTonnages.ToList();
+                listMaxTonnages.Add(-1);
+                listMaxTonnages.Add(-1);
+                listMaxTonnages.Add(-1);
+                listMaxTonnages.Add(-1);
+                listMinTonnages.Add(-1);
+                listMinTonnages.Add(-1);
+                listMinTonnages.Add(-1);
+                listMinTonnages.Add(-1);
+                slotMaxTonnages = listMaxTonnages.ToArray<float>();
+                slotMinTonnages = listMinTonnages.ToArray<float>();
+                AccessTools.Field(typeof(LanceConfiguratorPanel), "slotMaxTonnages").SetValue(__instance.LC, slotMaxTonnages);
+                AccessTools.Field(typeof(LanceConfiguratorPanel), "slotMinTonnages").SetValue(__instance.LC, slotMinTonnages);
+
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LanceConfiguratorPanel), "SetData")]
+    public static class LanceConfiguratorPanel_SetData {
+        static void Prefix(LanceConfiguratorPanel __instance, ref int maxUnits) {
+            try {
+                maxUnits = 8;
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(LanceConfiguratorPanel), "CreateLanceConfiguration")]
+    public static class LanceConfiguratorPanel_CreateLanceConfiguration {
+        static bool Prefix(LanceConfiguratorPanel __instance, ref LanceConfiguration __result) {
+            try {
+                return false;
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+                return false;
+            }
+        }
+
+        static void Postfix(LanceConfiguratorPanel __instance, ref LanceConfiguration __result, LanceLoadoutSlot[] ___loadoutSlots) {
+            try {
+                LanceConfiguration lanceConfiguration = new LanceConfiguration();
+                for (int i = 0; i < ___loadoutSlots.Length; i++) {
+                    LanceLoadoutSlot lanceLoadoutSlot = ___loadoutSlots[i];
+                    MechDef mechDef = null;
+                    PilotDef pilotDef = null;
+                    if (lanceLoadoutSlot.SelectedMech != null) {
+                        mechDef = lanceLoadoutSlot.SelectedMech.MechDef;
+                    }
+                    if (lanceLoadoutSlot.SelectedPilot != null) {
+                        pilotDef = lanceLoadoutSlot.SelectedPilot.Pilot.pilotDef;
+                    }
+                    if (mechDef != null && pilotDef != null) {
+                        if (i < 4) {
+                            lanceConfiguration.AddUnit(__instance.playerGUID, mechDef, pilotDef);
+                        } else {
+                            //EMPLOYER ID
+                            lanceConfiguration.AddUnit("ecc8d4f2-74b4-465d-adf6-84445e5dfc230", mechDef, pilotDef);
+                        }
+                    }
+                }
+                __result = lanceConfiguration;
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+            }
+        }
+    }
+
+   /* [HarmonyPatch(typeof(PlayerLanceSpawnerGameLogic), "ContractInitialize")]
+    public static class PlayerLanceSpawnerGameLogic_ContractInitialize {
+        static void Prefix(PlayerLanceSpawnerGameLogic __instance) {
+            try {
+                UnitSpawnPointGameLogic[] unitSpawnPointGameLogicList = __instance.unitSpawnPointGameLogicList;
+                SpawnableUnit[] lanceUnits = __instance.Combat.ActiveContract.Lances.GetLanceUnits("ecc8d4f2-74b4-465d-adf6-84445e5dfc230");
+                int num = 1;
+                while (num < lanceUnits.Length && num < unitSpawnPointGameLogicList.Length) {
+                    unitSpawnPointGameLogicList[num].OverrideSpawn(lanceUnits[num]);
+                    num++;
+                }
+            }
+            catch (Exception e) {
+                Logger.LogError(e);
+            }
+        }
+    }*/
+}
